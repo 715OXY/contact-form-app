05e49d9f57025417d4efa3ebea90d4d830761064
715OXY <sokushim@gmail.com>
2026-08-19 23:48:51 +0900

    タグCRUD実装 #12

---
+++ app/Http/Requests/StoreTagRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreTagRequest extends FormRequest
+{
+    /**
+     * リクエストの認可
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * バリデーションルール
+     * 
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:50',
+                Rule::unique('tags', 'name')->ignore($this->tag),
+            ],
+        ];
+    }
+    /**
+     * バリデーションメッセージ
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'タグ名を入力してください。',
+            'name.max' => 'タグ名は50文字以内で入力してください。',
+            'name.unique' => 'そのタグ名は既に使用されています。',
+        ];
+    }
+}
